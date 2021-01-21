cf26a76a530fdeea06b016f3d1274a0cef68af65
Francois Buys <buys.fran@gmail.com>
2021-01-21 19:23:13 +0200

    Add image create action specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
-  resources :images
+  resources :images, only: [:create, :show]
 end
--- spec/requests/images_request_spec.rb
+++ spec/requests/images_request_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "Images", type: :request do
+RSpec.describe 'Images', type: :request do
+  describe '.create' do
+    context 'with a valid image' do
+      it 'creates image and redirects to show' do
+        user = create(:user)
+        image = build(:image)
+        sign_in user
+        expect(Image.count).to eq(0)
 
+        post images_path, params: { image: { image: image.image } }
+
+        expect(response).to redirect_to(image_path(Image.last))
+        expect(Image.count).to eq(1)
+      end
+    end
+
+    context 'without a valid image' do
+      it 'does not create image and returns errors' do
+        user = create(:user)
+        invalid_image = Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/images/too_big.jpg'), 'image/jpg')
+        sign_in user
+
+        post images_path, params: { image: { image: invalid_image } }
+
+        expect(Image.count).to eq(0)
+        expect(controller.view_assigns['image'].errors).not_to be_empty
+      end
+    end
+  end
 end
