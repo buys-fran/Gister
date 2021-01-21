631bbe624c568527d030680378efb4df55ddbe6e
Francois Buys <buys.fran@gmail.com>
2021-01-21 18:45:43 +0200

    Add image validation specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,10 @@ FactoryBot.define do
     "person#{n}@gifster.com"
   end
 
+  factory :image do
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/images/test.png'), 'image/png') }
+  end
+
   factory :user do
     email
     password { 'password' }
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'with a valid image' do
+      it 'works' do
+        expect(create(:image).valid?).to eq(true)
+      end
+    end
+
+    context 'with a invalid image' do
+      it 'marks big images as invalid' do
+        big_image = Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/images/too_big.jpg'), 'image/jpg')
+        invalid_image = build(:image, image: big_image)
+        expect(invalid_image.valid?).to eq(false)
+      end
+    end
+  end
 end
